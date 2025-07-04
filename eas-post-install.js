db1e3935a78e750ea8c645b8809ef2483b3dace4
Flávio Sousa <flasoft@gmail.com>
2025-07-04 00:46:28 -0300

    Update post-install script

--- eas-post-install.js
+++ eas-post-install.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const { execSync } = require("child_process");
 
 try {
-  execSync("node encrypt-hymns.js encrypt --override", { stdio: "inherit" });
-  execSync("node encrypt-hymns.js encrypt Hymns.json --override", { stdio: "inherit" });
+  execSync("node encrypt-hymns.js encrypt --override", { stdio: "inherit", env: process.env });
+  execSync("node encrypt-hymns.js encrypt Hymns.json --override", { stdio: "inherit", env: process.env });
   console.log("eas-post-install completed successfully.");
 } catch (error) {
   console.error("eas-post-install failed:", error);
